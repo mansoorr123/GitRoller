47c986e00d8d41652c75c5c4517e2a52f673b196
Mansoor R <mansoorr@protonmail.com>
2020-01-13 23:18:42 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo Welcome to GitRoller Setup Wizard
+echo "**********************************"
+
+echo Installing truffleHog from github into current directory: 
+echo
+git clone https://github.com/dxa4481/truffleHog.git
+chmod +x truffleHog/truffleHog/truffleHog.py
+echo
+echo Installing truffleHog python module using pip:
+pip install truffleHog
+echo
+echo
+echo You have to configure truffleHog path in GitRoller.sh script.
+echo
+echo Installation Completed
